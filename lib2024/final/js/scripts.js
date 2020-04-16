677bdf9a622cb43b254e4533d5e16c836bcf72f4
gend0065 <59586716+gend0065@users.noreply.github.com>
2020-04-15 23:43:20 -0400

    js fix

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,67 +1,15 @@
-// document.querySelector('#color-1').style.backgroundColor="green";
+var subButton = document.querySelector("#subBtn");
 
-// window is the keyword targeting current browser
-// window.onload asks the browser to wait for the page to load and then run the function(called init in this case)
-window.onload = init;
-// this is done to avoid complications of function running without having the elements ready and rendered
+subButton.addEventListener('click', function() {
+	var name = document.querySelector("#name");
+	var email = document.querySelector("#email");
+	var address = document.querySelector("#address");
+	var city = document.querySelector("#city");
+	var province = document.querySelector("#province");
+	var postal = document.querySelector("#PostalCode");
 
-// function name-of-function will create a function in js file
-// function must have a opening curly brace and a closing one {} similar to css
-// the name of a function is followed by brackets ()
-function init() {
-	
-	document.querySelector('.ham').onclick = showHideMobileMenu;
+	var infoP = document.querySelector("#infoP");
 
+	infoP.innerHTML = `${name.value}<br>${email.value}<br>${address.value}, ${city.value},<br>${province.value}, ${postal.value}`;
+});
 
-	// JQuery function attached to the submit event of the form with id "form"
-	$('#form').submit(function (e) {
-		// e.preventDefualt to avoid the form being submitted to page specified in action attribute 
-   		 e.preventDefault();
-   		 // passing the current form (this) to variable form 
-   		 var form = this;
-   		 // fadeIn is a jQuery function to fadeIn an element 
-   		 $(".overlay-container").fadeIn(1000, function(){
-   		 		//call the showFormValues function and pass variable form to it as argument
-   		 		showformValues(form);
-   		 		// call the createTableFromJSON function
-   		 		createTableFromJSON();
-		   		 // fadeOut is a jQuery function to fadeOut an element 
-   		 		$('.overlay-container').delay(500).fadeOut(500);
-   		 })
-	});
-
-}
-
-// to show the form values in the results div which takes the argument "form"
-function showformValues(form){
-	//serializeArray is a jquery function used to get the values of a form as js Object
-	var formValues = $(form).serializeArray(); 
-	// $.each is a jquery alternative to for loop to iterate through an JS array or object  (Especially beneficial when the length of array is not known)
-	// index is the index  of the current element i.e 0,1,2,3 so on 
-		//field is the actual field being accessed 
-		
-	$.each(formValues, function(index, field){
-
-		// following code does the following : 
-		// 1) $("#results") -- (Gets the  selects the div with id results 
-		// 2) .fund("#"+field.name+"_result") -- finds the element with id equal to the name of the field being accessed along with text ("_result") Eg : name, pc_result, email_result
-		// 3) Modifies the text inside the selected element and replaces it with the value of this field   
-		$("#results").find("#"+field.name+"_result").text(field.value);
-
-		// special check for email to add a link instead of just string
-		if(field.name=="email"){
-			$("#results").find("#"+field.name+"_result").attr("href", "mailto:"+field.value);
-		}
-	})				
-}
-
-        // EXTRACT VALUE FOR HTML HEADER. 
-        // ('Student ID', 'Name', 'Email' and 'Marks')
-        var col = [];
-        for (var i = 0; i < myBooks.length; i++) {
-            for (var key in myBooks[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }
